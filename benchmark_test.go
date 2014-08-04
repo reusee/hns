cc7622b2e8ceb8756ad14aa1126d55579add020e
reus <reusee@gmail.com>
2014-08-04 21:39:16 +0800

    update benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,38 +1,36 @@
 package hns
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func getTestHtml() io.Reader {
+	f, err := os.Open("test.html")
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
 
 func BenchmarkParse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, err := ParseString(`
-<div>
-	<p>
-		<a>
-		</a>
-	</p>
-</div>
-	`)
+		_, err := Parse(getTestHtml())
 		if err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
-func BenchmarkCss(b *testing.B) {
-	root, err := ParseString(`
-<div>
-	<p>
-		<a>
-			<img />
-		</a>
-	</p>
-</div>
-	`)
+func BenchmarkWalk(b *testing.B) {
+	root, err := Parse(getTestHtml())
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
-		root.Walk(Css("div p a img", Return))
+		root.Walk(Css("a", Return))
 	}
 }
